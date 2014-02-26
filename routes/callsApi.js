1e31d6aac5dd4ab3ded79fc99da48f51793bd19d
unknown <Tomazini@UNIWEB_1.intranet.publisoft.com>
2014-02-26 13:53:04 -0300

    metodo update

--- routes/callsApi.js
+++ routes/callsApi.js
@@ -50,12 +50,14 @@ exports.findById = function(req, res) {
 	console.log('Retrieving mesa: ' + id);
 	db.collection('mesas', function(err, collection) {
 		collection.findOne({Id: id}, function(err, item) {
-			//res.send("Item ---> " + item);
+			res.send("Item ---> " + item);
 			console.log('Item ---> ' + item);
+			console.log('err ---> ' + err);
 		});
 		collection.findOne({'Id': id}, function(err, item) {
-			//res.send("2 - Item ---> " + item);
+			res.send("2 - Item ---> " + item);
 			console.log('Outro Item ---> ' + item);
+			console.log('Outro err ---> ' + err);
 		});
 	});
 };
