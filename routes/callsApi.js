7f9c6d6a5056bc69b5e35d0a7dc71d4e2571be20
unknown <Tomazini@UNIWEB_1.intranet.publisoft.com>
2014-03-05 08:59:55 -0300

    inclusao de id produto grupo

--- routes/callsApi.js
+++ routes/callsApi.js
@@ -387,10 +387,17 @@ var populateProduto = function() {
 	console.log("populando produto");    
 
 	var produto = [
-    {Id: 1, Codigo: '500', Descricao: 'Produto', },
-	{Id: 2, Codigo: '501', Descricao: 'Produto 2'},
-	{Id: 3, Codigo: '502', Descricao: 'Produto 3'},
-	{Id: 4, Codigo: '503', Descricao: 'Produto 4'}];
+    {Id: 1, ProdutoGrupoId: 1, Codigo: '500', Descricao: 'Produto 1'},
+	{Id: 2, ProdutoGrupoId: 1, Codigo: '501', Descricao: 'Produto 2'},
+	{Id: 3, ProdutoGrupoId: 2, Codigo: '600', Descricao: 'Produto 3'},
+	{Id: 4, ProdutoGrupoId: 2, Codigo: '601', Descricao: 'Produto 4'},	
+	{Id: 5, ProdutoGrupoId: 3, Codigo: '701', Descricao: 'Produto 5'},
+	{Id: 6, ProdutoGrupoId: 3, Codigo: '702', Descricao: 'Produto 6'},
+	{Id: 7, ProdutoGrupoId: 3, Codigo: '703', Descricao: 'Produto 7'},
+	{Id: 8, ProdutoGrupoId: 4, Codigo: '800', Descricao: 'Produto 8'},
+	{Id: 9, ProdutoGrupoId: null, Codigo: '700', Descricao: 'Produto Sem Grupo Null'},
+	{Id: 11, Codigo: '701', Descricao: 'Produto Sem Grupo Sem Enviar ProdutoGrupoId'},
+	{Id: 10, ProdutoGrupoId: 1, Codigo: '502', Descricao: 'Produto 2'}];
 	
 	db.collection('produto', function(err, collection) {
 		console.log("inserindo produto");
