b3749ad5d4c760b6bc1cf18db53f3d5851cefef8
unknown <Tomazini@UNIWEB_1.intranet.publisoft.com>
2014-03-10 17:36:40 -0300

    #100320141736

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ var app = express();
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     //app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
+    
+	//app.use(express.bodyParser()); // deprecated
+	
+	app.use(express.json());
+	app.use(express.urlencoded());
+
     //app.use(express.static(path.join(__dirname, 'public')));
 });
 
