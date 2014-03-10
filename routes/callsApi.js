59afaa4a96da625186cb69f017b51b05fc356849
unknown <Tomazini@UNIWEB_1.intranet.publisoft.com>
2014-03-10 10:07:44 -0300

    add consumo

--- routes/callsApi.js
+++ routes/callsApi.js
@@ -127,14 +127,14 @@ exports.atualizarmesa = function(req, res) {
 };
 
 exports.addconsumomesa = function(req, res) {
-	console.log('body: ' + req.body);	
+	console.log('req: ' + req);	
 	
 	var idmesa = req.param("mesaid"); // mesaid=1&deviceid=2&produtoid=2&quantidade=1
 	var iddevice = req.param("deviceid");
 	var idproduto = req.param("produtoid");
 	var quantidade = req.param("quantidade");
 	
-	//console.log('Adding stringify: ' + JSON.stringify(wine));
+	console.log('Adding stringify: ' + JSON.stringify(req));
 	
 	//console.log('req: ' + req);
 	//console.log('parse req: ' + JSON.parse(req));
